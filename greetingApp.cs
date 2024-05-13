2620260765cded72ebca9174590c0f87098e1086
dcerutti1 <48871871+dcerutti1@users.noreply.github.com>
2024-05-13 15:40:50 -0500

    Add files via upload

---
+++ greetingApp.cs
@@ -0,0 +1,21 @@
+﻿
+
+//  welcome user to app
+Console.WriteLine("Welcome to the Greeting App!");
+Console.WriteLine("this was built by Dorian Cerutti");
+Console.WriteLine("---------------------------------");
+Console.WriteLine();
+
+//  ask for first name
+Console.Write("What is your first name: ");
+string? firstName;
+firstName = Console.ReadLine();
+
+
+//  greet user by name
+Console.WriteLine(  );
+Console.WriteLine($"Hello {firstName}");
+
+
+Console.WriteLine("Thank you for using my basic app!");
+Console.ReadLine();
